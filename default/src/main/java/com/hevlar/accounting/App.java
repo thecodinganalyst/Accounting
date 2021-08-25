245f9e76ad670ae0683ea42bc89ed3f827e5e528
Dennis Cai <iamdenniscai@gmail.com>
2021-08-25 16:04:01 +0800

    removed chart of accounts from base

--- default/src/main/java/com/hevlar/accounting/App.java
+++ default/src/main/java/com/hevlar/accounting/App.java
@@ -2,8 +2,6 @@ package com.hevlar.accounting;
 
 import com.hevlar.accounting.controller.ChartOfAccounts;
 
-import java.time.LocalDate;
-
 public class App{
 
     public static void main(String[] args){
