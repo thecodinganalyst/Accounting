3bfda1654657b142cdbad1415f65942bbcb9cb03
Dennis Cai <iamdenniscai@gmail.com>
2021-08-25 10:16:17 +0800

    add journal entry

--- src/main/java/com/hevlar/accounting/model/JournalEntry.java
+++ src/main/java/com/hevlar/accounting/model/JournalEntry.java
@@ -1,7 +1,5 @@
 package com.hevlar.accounting.model;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Currency;
@@ -25,9 +23,24 @@ public class JournalEntry {
     private LocalDate postDate;
     private LocalDate debitStatementDate;
     private LocalDate creditStatementDate;
-    private AtomicBoolean locked;
+    private final AtomicBoolean locked;
 
-    public JournalEntry(AtomicLong journalId, LocalDate txDate, String item, Recurrence recurrence, String[] tags, String currency, Double amount, Account debit, Account credit, LocalDate postDate, LocalDate debitStatementDate, LocalDate creditStatementDate) {
+    /**
+     * Default constructor
+     * @param journalId unique identifier
+     * @param txDate date when transaction is made
+     * @param item description of the transaction
+     * @param recurrence state whether this is a recurring transaction and how often is the recurrence
+     * @param tags the tags used to search for this entry
+     * @param currency ISO 4217 code of the currency
+     * @param amount amount charged for this entry
+     * @param debit debit account
+     * @param credit credit account
+     * @param postDate date when transaction is posted, usually for banks and credit cards, when transaction is not posted on non-working days
+     * @param debitStatementDate for credit cards, to identify which statement should this entry appear for the debit account
+     * @param creditStatementDate for credit cards, to identify which statement should this entry appear for the credit account
+     */
+    public JournalEntry(AtomicLong journalId, LocalDate txDate, String item, Recurrence recurrence, String[] tags, String currency, String amount, Account debit, Account credit, LocalDate postDate, LocalDate debitStatementDate, LocalDate creditStatementDate) {
         this.journalId = journalId;
         this.txDate = txDate;
         this.item = item;
@@ -43,104 +56,258 @@ public class JournalEntry {
         this.locked = new AtomicBoolean(false);
     }
 
+    /**
+     * Gets the journal id
+     * @return journal id
+     */
     public AtomicLong getJournalId() {
         return journalId;
     }
 
+    /**
+     * Gets the transaction date
+     * @return transaction date
+     */
     public LocalDate getTxDate() {
         return txDate;
     }
 
+    /**
+     * Gets the description of the journal entry
+     * @return description of journal entry
+     */
     public String getItem() {
         return item;
     }
 
+    /**
+     * Gets the recurrence
+     * @return recurrence
+     */
     public Recurrence getRecurrence() {
         return recurrence;
     }
 
+    /**
+     * Gets the tags associated with this entry
+     * @return tags
+     */
     public String[] getTags() {
         return tags;
     }
 
+    /**
+     * Gets the currency associated with this entry
+     * @return currency
+     */
     public Currency getCurrency() {
         return currency;
     }
 
+    /**
+     * Gets the amount
+     * @return amount
+     */
     public BigDecimal getAmount() {
         return amount;
     }
 
+    /**
+     * Gets the debit account of this entry
+     * @return debit account
+     */
     public Account getDebit() {
         return debit;
     }
 
+    /**
+     * Gets the credit account of this entry
+     * @return credit account
+     */
     public Account getCredit() {
         return credit;
     }
 
+    /**
+     * Gets the posted date of this entry
+     * @return posted date
+     */
     public LocalDate getPostDate() {
         return postDate;
     }
 
+    /**
+     * Gets the statement date of the debit account for credit card
+     * @return statement date of debit account
+     */
     public LocalDate getDebitStatementDate() {
         return debitStatementDate;
     }
 
+    /**
+     * Gets the statement date of the credit account for the credit card
+     * @return statement date of credit account
+     */
     public LocalDate getCreditStatementDate() {
         return creditStatementDate;
     }
 
+    /**
+     * Check if the entry is locked
+     * @return locked status
+     */
     public Boolean isLocked(){
         return locked.get();
     }
 
+    /**
+     * Locks this entry from being edited
+     */
     public void lock(){
         locked.set(true);
     }
 
-    public void setTxDate(LocalDate txDate) {
+    /**
+     * Update the transaction date
+     * @param txDate transaction date
+     * @return true if successful, false otherwise
+     */
+    public Boolean setTxDate(LocalDate txDate) {
+        if(isLocked()) return false;
         this.txDate = txDate;
+        return true;
     }
 
-    public void setItem(String item) {
+    /**
+     * Update the description of the entry
+     * @param item description of the entry
+     * @return true if successful, false otherwise
+     */
+    public Boolean setItem(String item) {
+        if(isLocked()) return false;
         this.item = item;
+        return true;
     }
 
-    public void setRecurrence(Recurrence recurrence) {
+    /**
+     * Update the recurrence
+     * @param recurrence recurrence
+     * @return true if successful, false otherwise
+     */
+    public Boolean setRecurrence(Recurrence recurrence) {
+        if(isLocked()) return false;
         this.recurrence = recurrence;
+        return true;
     }
 
-    public void setTags(String[] tags) {
+    /**
+     * Update the tags of this entry
+     * @param tags tags associated with this entry
+     * @return true if successful, false otherwise
+     */
+    public Boolean setTags(String[] tags) {
+        if(isLocked()) return false;
         this.tags = tags;
+        return true;
     }
 
-    public void setCurrency(Currency currency) {
+    /**
+     * Updates the currency of this entry
+     * @param currency currency
+     * @return true if successful, false otherwise
+     */
+    public Boolean setCurrency(Currency currency) {
+        if(isLocked()) return false;
         this.currency = currency;
+        return true;
     }
 
-    public void setAmount(BigDecimal amount) {
+    /**
+     * Updates the currency of this entry by currency code
+     * @param currency currency code
+     * @return true if successful, false otherwise
+     */
+    public Boolean setCurrency(String currency) {
+        if(isLocked()) return false;
+        this.currency = Currency.getInstance(currency);
+        return true;
+    }
+
+    /**
+     * Updates the amount of this entry
+     * @param amount bigdecimal amount
+     * @return true if successful, false otherwise
+     */
+    public Boolean setAmount(BigDecimal amount) {
+        if(isLocked()) return false;
         this.amount = amount;
+        return true;
     }
 
-    public void setDebit(Account debit) {
+    /**
+     * Updates the amount of this entry by string value
+     * @param amount amount
+     * @return true if successful, false otherwise
+     */
+    public Boolean setAmount(String amount) {
+        if(isLocked()) return false;
+        this.amount = new BigDecimal(amount);
+        return true;
+    }
+
+    /**
+     * Updates the debit account
+     * @param debit debit account
+     * @return true if successful, false otherwise
+     */
+    public Boolean setDebit(Account debit) {
+        if(isLocked()) return false;
         this.debit = debit;
+        return true;
     }
 
-    public void setCredit(Account credit) {
+    /**
+     * Updates the credit account
+     * @param credit credit account
+     * @return true if successful, false otherwise
+     */
+    public Boolean setCredit(Account credit) {
+        if(isLocked()) return false;
         this.credit = credit;
+        return true;
     }
 
-    public void setPostDate(LocalDate postDate) {
+    /**
+     * Upates the post date of the entry
+     * @param postDate posted date of entry
+     * @return true if successful, false otherwise
+     */
+    public Boolean setPostDate(LocalDate postDate) {
+        if(isLocked()) return false;
         this.postDate = postDate;
+        return true;
     }
 
-    public void setDebitStatementDate(LocalDate debitStatementDate) {
+    /**
+     * Updates the debit statement date of this entry
+     * @param debitStatementDate debit statement date
+     * @return true if successful, false otherwise
+     */
+    public Boolean setDebitStatementDate(LocalDate debitStatementDate) {
+        if(isLocked()) return false;
         this.debitStatementDate = debitStatementDate;
+        return true;
     }
 
-    public void setCreditStatementDate(LocalDate creditStatementDate) {
+    /**
+     * Updates the credit statement date of this entry
+     * @param creditStatementDate credit statement date
+     * @return true if successful, false otherwise
+     */
+    public Boolean setCreditStatementDate(LocalDate creditStatementDate) {
+        if(isLocked()) return false;
         this.creditStatementDate = creditStatementDate;
+        return true;
     }
 
 }
--- src/test/java/com/hevlar/accounting/model/JournalEntryTest.java
+++ src/test/java/com/hevlar/accounting/model/JournalEntryTest.java
@@ -1,11 +1,15 @@
 package com.hevlar.accounting.model;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Currency;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,8 +27,133 @@ class JournalEntryTest {
 
     @BeforeEach
     void setUp() {
-        je = new JournalEntry(new AtomicLong(1), LocalDate.now(), "Lunch", Recurrence.N, null, "SGD", 10.0, expense, cash, null, null, null);
+        je = new JournalEntry(new AtomicLong(1), LocalDate.now(), "Lunch", Recurrence.N, null, "SGD", "10.0", expense, cash, null, null, null);
     }
 
+    @Test
+    void lock() {
+        assertFalse(je.isLocked());
+        je.lock();
+        assertTrue(je.isLocked());
+    }
 
+    @Test
+    void setTxDate() {
+        assertTrue(je.setTxDate(LocalDate.of(2021, 12, 12)));
+        assertEquals(je.getTxDate(), LocalDate.of(2021, 12, 12));
+        je.lock();
+        assertFalse(je.setTxDate(LocalDate.of(2021, 6, 6)));
+        assertEquals(je.getTxDate(), LocalDate.of(2021, 12, 12));
+    }
+
+    @Test
+    void setItem() {
+        assertTrue(je.setItem("Item"));
+        assertEquals(je.getItem(), "Item");
+        je.lock();
+        assertFalse(je.setItem("Test"));
+        assertEquals(je.getItem(), "Item");
+    }
+
+    @Test
+    void setRecurrence() {
+        assertTrue(je.setRecurrence(Recurrence.D));
+        assertEquals(je.getRecurrence(), Recurrence.D);
+        je.lock();
+        assertFalse(je.setRecurrence(Recurrence.M));
+        assertEquals(je.getRecurrence(), Recurrence.D);
+    }
+
+    @Test
+    void setTags() {
+        assertTrue(je.setTags(new String[]{"#food", "#lunch"}));
+        assertEquals(je.getTags().length, 2);
+        assertArrayEquals(je.getTags(), new String[]{"#food", "#lunch"});
+        je.lock();
+        assertFalse(je.setTags(new String[]{"#test"}));
+        assertEquals(je.getTags().length, 2);
+    }
+
+    @Test
+    void setCurrency_by_code() {
+        assertTrue(je.setCurrency("CNY"));
+        assertEquals(je.getCurrency().getCurrencyCode(), "CNY");
+        je.lock();
+        assertFalse(je.setCurrency("USD"));
+        assertEquals(je.getCurrency().getCurrencyCode(), "CNY");
+    }
+
+    @Test
+    void setCurrency() {
+        assertTrue(je.setCurrency(Currency.getInstance("MYR")));
+        assertEquals(je.getCurrency().getCurrencyCode(), "MYR");
+        je.lock();
+        assertFalse(je.setCurrency("USD"));
+        assertEquals(je.getCurrency().getCurrencyCode(), "MYR");
+    }
+
+    @Test
+    void setAmount() {
+        assertTrue(je.setAmount(new BigDecimal("3.2")));
+        assertEquals(je.getAmount(), new BigDecimal("3.2"));
+        je.lock();
+        assertFalse(je.setAmount(new BigDecimal("2.1")));
+        assertEquals(je.getAmount(), new BigDecimal("3.2"));
+    }
+
+    @Test
+    void setAmount_by_string() {
+        assertTrue(je.setAmount("4.1"));
+        assertEquals(je.getAmount(), new BigDecimal("4.1"));
+        je.lock();
+        assertFalse(je.setAmount(new BigDecimal("2.1")));
+        assertEquals(je.getAmount(), new BigDecimal("4.1"));
+    }
+
+    @Test
+    void setDebit() {
+        IncomeStatementAccount food = Mockito.mock(IncomeStatementAccount.class);
+        Mockito.when(food.getName()).thenReturn("food");
+        assertTrue(je.setDebit(food));
+        assertEquals(je.getDebit().getName(), "food");
+        je.lock();
+        assertFalse(je.setDebit(food));
+    }
+
+    @Test
+    void setCredit() {
+        BalanceSheetAccount bank = Mockito.mock(BalanceSheetAccount.class);
+        Mockito.when(bank.getName()).thenReturn("Bank");
+        assertTrue(je.setCredit(bank));
+        assertEquals(je.getCredit().getName(), "Bank");
+        je.lock();
+        assertFalse(je.setCredit(bank));
+    }
+
+    @Test
+    void setPostDate() {
+        assertTrue(je.setPostDate(LocalDate.of(2021, 12, 12)));
+        assertEquals(je.getPostDate(), LocalDate.of(2021, 12, 12));
+        je.lock();
+        assertFalse(je.setPostDate(LocalDate.of(2021, 6, 6)));
+        assertEquals(je.getPostDate(), LocalDate.of(2021, 12, 12));
+    }
+
+    @Test
+    void setDebitStatementDate() {
+        assertTrue(je.setDebitStatementDate(LocalDate.of(2021, 12, 12)));
+        assertEquals(je.getDebitStatementDate(), LocalDate.of(2021, 12, 12));
+        je.lock();
+        assertFalse(je.setDebitStatementDate(LocalDate.of(2021, 6, 6)));
+        assertEquals(je.getDebitStatementDate(), LocalDate.of(2021, 12, 12));
+    }
+
+    @Test
+    void setCreditStatementDate() {
+        assertTrue(je.setCreditStatementDate(LocalDate.of(2021, 12, 12)));
+        assertEquals(je.getCreditStatementDate(), LocalDate.of(2021, 12, 12));
+        je.lock();
+        assertFalse(je.setCreditStatementDate(LocalDate.of(2021, 6, 6)));
+        assertEquals(je.getCreditStatementDate(), LocalDate.of(2021, 12, 12));
+    }
 }
